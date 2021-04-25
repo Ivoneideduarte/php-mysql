695c571a0690d77d2eefe1787809abd6d8feb9d7
Ivoneide Duarte <contato.ivoneideduarte@gmail.com>
2021-04-25 16:47:31 -0300

    Exemplo de hora em php

---
+++ 02 - Construindo um calendário com PHP/calendario.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendário com PHP</title>
+</head>
+<body>
+<div>
+    <table border="1">
+        <tr>
+            <th>Dom</th>
+            <th>Seg</th>
+            <th>Ter</th>
+            <th>Qua</th>
+            <th>Qui</th>
+            <th>Sex</th>
+            <th>Sáb</th>
+        </tr>
+    </table>
+</div>
+</body>
+</html>
