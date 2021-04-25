e32f28ec278a4ea5dfb4fd5b47cb9f76200d35af
Ivoneide Duarte <contato.ivoneideduarte@gmail.com>
2021-04-25 19:50:58 -0300

    Criando um formulário

---
+++ 03 - Entrada de dados com formulário/tarefas.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerenciador de tarefas</title>
+</head>
+<body>
+    <h1>Gerenciador de tarefas</h1>
+    <form action="">
+        <fieldset>
+            <legend>Nova tarefa</legend>
+            <label for="">
+                Tarefa: <input type="text" name="nome"/>
+            </label>
+            <input type="submit" value="Cadastrar"/>
+        </fieldset>
+    </form>
+</body>
+</html>
