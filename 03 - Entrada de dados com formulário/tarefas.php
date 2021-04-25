7b7b3e9a02fe795fcce7d7c5340f411ea16f7e01
Ivoneide Duarte <contato.ivoneideduarte@gmail.com>
2021-04-25 20:25:50 -0300

    Gerenciador de tarefas finalizado

--- 03 - Entrada de dados com formulário/tarefas.php
+++ 03 - Entrada de dados com formulário/tarefas.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -17,5 +18,37 @@
             <input type="submit" value="Cadastrar"/>
         </fieldset>
     </form>
+    <?php
+        /*
+        if (isset($_GET['nome']))
+        { // isset() Verifica se uma variável existe 
+            echo "Nome informado: " . $_GET['nome'];
+        }
+        */
+        $lista_tarefas = array();
+
+        if (isset($_GET['nome']))
+        { // Verifica se existe o índice 'nome' dentro de $_GET
+            $_SESSION['lista_tarefas'][] = $_GET['nome']; // $_SESSION serve para escrever e ler  informações e mantér os dados
+        }
+
+        $lista_tarefas = array();
+
+        if (isset($_SESSION['lista_tarefas']))
+        {
+            $lista_tarefas = $_SESSION['lista_tarefas'];
+        }
+    ?>
+    <table>
+        <tr>
+            <th>Tarefas</th>
+        </tr>
+            <?php foreach ($lista_tarefas as $tarefa) : ?>
+            <!-- Este laço serve para passar por todos os índices de um array, atribuindo cada índice a uma variável que escolhemos, no caso, a variável $tarefa -->
+                <tr>
+                    <td><?php echo $tarefa; ?></td>
+                </tr>
+            <?php endforeach; ?>
+    </table>
 </body>
 </html>
